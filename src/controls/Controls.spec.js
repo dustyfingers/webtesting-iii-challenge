b7e6c39c5c1e89b17c4ee99333adff406ef85e91
dustyfingers <dustyfingers01257@gmail.com>
2019-08-19 21:47:49 -0600

    added tests for controls and dashboard

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,50 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+    it('should display unlock gate and disable open button if locked is true', () => {
+        const { getByText } = render(<Controls locked />);
+        getByText(/unlock gate/i);
+        const openButton = getByText(/close gate/i);
+        expect(openButton).toBeDisabled();
+    });
+
+    it('should display lock gate if locked is false', () => {
+        const { getByText } = render(<Controls locked={false} />);
+        getByText(/lock gate/i);
+    });
+
+    it('should display open gate if the closed is true', () => {
+        const { getByText } = render(<Controls closed />);
+        getByText(/open gate/i);
+    });
+
+    it('should display close gate and disable lock button if closed is false', () => {
+        const { getByText } = render(<Controls closed={false} />);
+        getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+        expect(lockButton).toBeDisabled();
+    });
+    it('should fire click handler functions', () => {
+        const toggleLocked = jest.fn();
+        const toggleClosed = jest.fn();
+
+        const { getByText } = render(
+            <Controls
+                closed
+                locked={false}
+                toggleLocked={toggleLocked}
+                toggleClosed={toggleClosed}
+            />
+        );
+        const openButton = getByText(/open gate/i);
+        const lockButton = getByText(/lock gate/i);
+        fireEvent.click(openButton);
+        fireEvent.click(lockButton);
+        expect(toggleLocked).toHaveBeenCalledTimes(1);
+        expect(toggleClosed).toHaveBeenCalledTimes(1);
+    });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Dashboard from './Dashboard';
 
 describe('<Dashboard />', () => {
-    it('should match snapshot', () => {
-        const tree = renderer.create(<Dashboard />).toJSON();
 
-        expect(tree).toMatchSnapshot();
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Dashboard />);
+        expect(tree.toJSON()).toMatchSnapshot();
     });
+
+    it('should render controls and display', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/unlocked/i);
+        getByText(/open/i);
+        getByText(/lock Gate/i);
+        getByText(/close Gate/i);
+    });
+
 });
